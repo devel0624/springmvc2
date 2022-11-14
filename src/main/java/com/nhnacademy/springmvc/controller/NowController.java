19f0863caede0ebace32260b6e18acf89160a379
LSJ <devel0624@gmail.com>
2022-11-14 12:26:24 +0900

    commit initializer

--- src/main/java/com/nhnacademy/springmvc/controller/NowController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/NowController.java
@@ -2,11 +2,21 @@ package com.nhnacademy.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class NowController {
     @GetMapping("/now")
     public String now() {
         return "now";
+    }
+
+    @GetMapping("/now2")
+    public ModelAndView doSomething() {
+        ModelAndView mav = new ModelAndView("viewName");
+        mav.addObject("name", "value");
+        // ...
+
+        return mav;
     }
 }
